95f31e401162137d91813bcf86b3bb5bc821c9aa
Vitus Maierhöfer <vitus.maierhoefer@gmail.com>
2019-04-23 15:50:55 +0200

    Prefill combobox and extract values from input fields
    The combobox now displays a list of all possible values. When the "Start measurement" button is pressed, the values from all input fields are extracted to verify them.

--- gui/latency_gui_new.py
+++ gui/latency_gui_new.py
@@ -4,18 +4,38 @@
 from PyQt5 import QtWidgets, uic
 import sys
 
+class Constants:
+    DEVICE_TYPES = ["Mouse", "Keyboard", "Gamepad"]
+
 
 class LatencyGUI(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
-        print("Hallo")
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
+    def init_ui(self):
         self.ui = uic.loadUi("latency_gui.ui", self)
         self.show()
+        self.init_combobox()
+        self.ui.button_start_measurement.clicked.connect(self.on_measurement_started_button_pressed)
 
+    def init_combobox(self):
+        self.ui.comboBox_device_type.addItems(Constants.DEVICE_TYPES)
+        self.ui.comboBox_device_type.setCurrentIndex(0)
+
+    def on_measurement_started_button_pressed(self):
+        print("Starting measurement")
+        self.validate_inputs()
+
+    def validate_inputs(self):
+        authors = self.ui.lineEdit_authors.text()
+        device_name = self.ui.lineEdit_device_name.text()
+        device_type = str(self.ui.comboBox_device_type.currentText())
+
+        print("Authors: ", authors)
+        print("Device name: ", device_name)
+        print("Device type: ", device_type)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
