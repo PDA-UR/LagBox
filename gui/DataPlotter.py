3722bc1d4ada0a59bb55d2dfa3ef10aa68fdb7c6
Vitus Maierhöfer <vitus.maierhoefer@gmail.com>
2019-05-06 17:29:24 +0200

    Plot: Fix font scale and x-Axis range + Code cleanup

--- gui/DataPlotter.py
+++ gui/DataPlotter.py
@@ -8,41 +8,17 @@ import numpy as np
 class Constants:
     CSV_DELIMITER = ';'
     PLOT_X_MIN = 0  # Minimum x value of the plot
-    PLOT_X_MAX = 100  # Maximum x value of the plot
+    # PLOT_X_MAX = 100  # Maximum x value of the plot
     PLOT_WIDTH = 9
     PLOT_HEIGHT = 2
     PLOT_OUTPUT_DPI = 300
-    PLOT_FONTSIZE = 12
-
-
-class Result:
-
-    def __init__(self):
-        self.name = ''
-        self.minDelay = 100
-        self.maxDelay = 10000
-        self.iterations = 1000
-        self.authors = ''
-        self.vendorID = ''
-        self.productID = ''
-        self.date = ''
-        self.bInterval = 1000
-        self.deviceType = 'Mouse'
-        self.mean = 0.0
-        self.median = 0.0
-        self.min = 0.0
-        self.max = 0.0
-        self.standardDeviation = 0.0
+    PLOT_FONTSIZE = 16
 
 
 class DataPlotter:
 
-    result = Result()
-
     # Reads in a csv file and hands the data over at the end
     def process_filedata(self, file_path):
-
-        print(file_path)
 
         try:
             current_file = open(file_path, 'r').readlines()  # Open the csv File
@@ -60,13 +36,8 @@ class DataPlotter:
                 measurement_rows = current_file[i + 1:len(current_file)]
                 break  # No need to continue the loop
 
-        # print(comment_lines)
-        # print(measurement_rows)
-
         # The csv file is now read in and relevant parts are extracted. Now the data needs to be processed further
         latencies = self.parse_measurements(measurement_rows)
-
-        self.parse_comments(comment_lines)
         stats = self.get_stats_about_data(latencies)
         self.generate_plot(file_path, latencies)
 
@@ -80,31 +51,13 @@ class DataPlotter:
             row_values = measurement_rows[i].split(Constants.CSV_DELIMITER)
             latencies.append(float(row_values[1]) / 1000)  # Divide by 1000 to get ms
 
-        for i in range(len(latencies)):
-            if latencies[i] > Constants.PLOT_X_MAX:  # Check if values will get clipped
-                print("WARNING: One or more measured latencies exceed the defined limit of the plots x-axis and will "
-                      "not be displayed!")
-                break
+        # for i in range(len(latencies)):
+        #     if latencies[i] > Constants.PLOT_X_MAX:  # Check if values will get clipped
+        #         print("WARNING: One or more measured latencies exceed the defined limit of the plots x-axis and will "
+        #               "not be displayed!")
+        #         break
 
         return latencies
-
-    # Interpret the comment lines of the .csv file. They contain relevant metadata about the measurement
-    def parse_comments(self, comment_lines):
-        print("Comments:")
-        for comment_line in comment_lines:
-            key = comment_line.split(';')[0].replace('#', '')
-            value = comment_line.split(';')[1]
-            print(key, value)
-            if key == "Device":
-                self.result.name = value
-            elif key == "minDelay":
-                self.result.minDelay = value
-            elif key == "maxDelay":
-                self.result.maxDelay = value
-            elif key == "iterations":
-                self.result.iterations = value
-            elif key == "":  # TODO: ADD ALL OTHER METADATA
-                pass
 
     # Calculate mean, median, standard deviation, etc.
     def get_stats_about_data(self, latencies):
@@ -115,12 +68,6 @@ class DataPlotter:
         standard_deviation = np.std(latencies)
 
         # TODO: Calculate additional stats (maybe ttest, ...)
-
-        self.result.mean = mean
-        self.result.median = median
-        self.result.min = minimum
-        self.result.max = maximum
-        self.result.standardDeviation = standard_deviation
 
         return ('Mean: ' + str(round(mean, 3)) +
                 ' Median: ' + str(round(median, 3)) +
@@ -138,7 +85,7 @@ class DataPlotter:
             print('Pyplot and/or Seaborn not installed')
             return
 
-        plt.rcParams.update({'font.size': Constants.PLOT_FONTSIZE})
+        #plt.rcParams.update({'font.size': Constants.PLOT_FONTSIZE})
         plt.figure(figsize=[Constants.PLOT_WIDTH, Constants.PLOT_HEIGHT])
 
         # ax = sns.pointplot((values["latency"]), values["polling"], join=False, palette="dark", markers="D", scale=.75,
@@ -146,13 +93,17 @@ class DataPlotter:
         # ax = sns.swarmplot((values["latency"]), values["polling"], hue=None, palette="colorblind", size=1, dodge=True,
         # marker="H",orient="h", alpha=1, zorder=0)
 
+        sns.set(font_scale=1.5)
+
+        # TODO: If we could guarantee here that at least seaborn version 0.9 is installed, we should add "dodge=True"
         ax = sns.swarmplot(x=latencies, hue=None, palette="colorblind", marker="H", orient="h", alpha=1,
                            zorder=0)
 
         # plt.title("TEST")
         plt.xlabel("latency (ms)")
-        plt.xlim(Constants.PLOT_X_MIN, Constants.PLOT_X_MAX)
-        # plt.xlim(left=0)
+        #plt.xlim(Constants.PLOT_X_MIN, Constants.PLOT_X_MAX)
+
+        plt.xlim(0, max(latencies) * 1.1)  # Leave 10% empty space on the right
 
         axes = plt.gca()
 
